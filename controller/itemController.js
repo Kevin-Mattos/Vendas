f75a1c5a0b2e80c09755613c26df4c45c37a187f
Kevin-Mattos <kevinmmattos@gmail.com>
2020-11-21 18:08:14 -0300

    Chamadas para o banco mais genericas, middleware para enviar resposta padrinizada

--- controller/itemController.js
+++ controller/itemController.js
@@ -1,74 +1,32 @@
-let db = require('../db').getdb()
+
 
 const table = 'item'
+let dbHanlders = require("../handlers")
+
 
 function getAll(req, res, next){
-    db.all(`SELECT * FROM ${table}`,function(err, rows){
-        if(err){
-            console.log(err)
-            res.send(err)
-            return
-        }
-        
-        res.send(rows)
-
-    })
+    dbHanlders.selectAllFromTable(table, req, res, next)
 }
 
 function insert(req, res, next){
     let body = req.body
-    let nome = body.nome
-  
+    let nome = body.nome  
     
-    db.run(`INSERT INTO ${table}(nome) VALUES (?)`, nome, function(err){
-        if(err){
-            res.statusCode = 400
-            res.send({'erro': err})
-            console.log(err)
-            return
-        }               
-        console.log(`${nome} inserido`)
-        res.send({"id": this.lastID, nome})
-    })
+    item = {nome}
+    dbHanlders.insertIntotable(table, item, req, res, next)
 }
 
 function remove(req, res, next){
     let body = req.body
-    let item = req.params.id    
+    let itemId = req.params.id    
 
-    db.run(`DELETE FROM ${table} where ${table}.id = ${item}` , function(err){
-        if(err || this.changes == 0){
-            res.statusCode = 400
-
-            if(!err)
-                res.send({'erro': 'item nao encontrado'})
-            else
-                res.send(err)
-                return
-        }      
-        
-        res.send({'removido': item})
-    })
+    dbHanlders.removeFromTable(table, itemId, req, res, next)
 }
 
 
-function getById(req, res, next){
-       
-    let item = req.params.id        
-
-    db.all(`SELECT * FROM ${table} where ${table}.id = ${item}` , function(err, rows){
-        if(err || !rows){
-            res.statusCode = 400
-
-            if(!err)
-                res.send({'erro': `${item} nao encontrado`})
-            else
-                res.send(err)
-            return
-        }      
-        console.log(rows)
-        res.send(rows)
-    })
+function getById(req, res, next){       
+    let itemId = req.params.id
+    dbHanlders.getById(table, itemId, req, res, next)
 }
 
 
--- controller/vendedoraController.js
+++ controller/vendedoraController.js
@@ -1,71 +1,29 @@
-let db = require('../db').getdb()
+
 const table = 'vendedora'
+let dbHanlders = require("../handlers")
+
 
 function getAll(req, res, next){
-    db.all(`SELECT * FROM ${table}`,function(err, rows){
-        if(err){
-            console.log(err)
-            res.send(err)
-            return
-        }
-              
-        res.send(rows)
-
-    })
+    dbHanlders.selectAllFromTable(table, req, res, next)
 }
 
 function insert(req, res, next){
     let body = req.body
-    let vendedora = body.nome
+    let vendedora = {"nome": body.nome}
     console.log(body)
-    db.run(`INSERT INTO ${table}(nome) VALUES (?)`, vendedora, function(err){
-        if(err){
-            res.statusCode = 400
-            res.send({'erro': err})
-            return
-        }       
-        
-        console.log(`${this.lastID} inserida`)
-        res.send({'id': this.lastID,'nome' :vendedora})
-    })
+
+    dbHanlders.insertIntotable(table, vendedora, req, res, next)
+   
 }
 
 function remove(req, res, next){
-
-    let vendedoraId = req.params.id    
-    console.log('i am here')
-    db.run(`DELETE FROM ${table} where ${table}.id = ${vendedoraId}` , function(err){
-        if(err || this.changes == 0){
-            res.statusCode = 400
-
-            if(!err)
-                res.send({'erro': `${table} nao encontrada`})
-            else
-                res.send(err)
-                return
-        }      
-        
-        res.send({'removido': vendedoraId})
-    })
+    let vendedoraId = req.params.id 
+    dbHanlders.removeFromTable(table, vendedoraId, req, res, next)
 }
 
-function getById(req, res, next){
-       
-    let vendedoraId = req.params.id        
-
-    db.all(`SELECT * FROM ${table} where ${table}.id = ${vendedoraId}` , function(err, rows){
-        if(err || !rows){
-            res.statusCode = 400
-
-            if(!err)
-                res.send({'erro': `${vendedoraId} nao encontrada`})
-            else
-                res.send(err)
-            return
-        }   
-        
-        res.send(rows)
-    })
+function getById(req, res, next){       
+    let vendedoraId = req.params.id      
+    dbHanlders.getById(table, vendedoraId, req, res, next)   
 }
 
 
