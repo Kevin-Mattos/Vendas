b3dfa3c8c55074a07ea8e17f3886ee5610b7d0a3
Kevin-Mattos <kevinmmattos@gmail.com>
2020-11-22 19:42:21 -0300

    sqlite deletar cascade

--- db.js
+++ db.js
@@ -7,10 +7,12 @@ const db = new sql.Database('./dbs/lojadamae.db', sql.OPEN_READWRITE | sql.OPEN_
     console.log('Connected to the users database.');
 })
 
-//db.run('drop table item')
-//db.run('drop table vendedora')
-//db.run('drop table venda')
-//db.run('drop table tipo')
+// db.run('drop table item')
+//  db.run('drop table vendedora')
+//  db.run('drop table venda')
+//  db.run('drop table tipo')
+
+db.run('PRAGMA foreign_keys = ON')
 
 let queryCriaVendedora = `CREATE TABLE IF NOT EXISTS
                 vendedora(
@@ -35,8 +37,8 @@ let queryCriaItem = `CREATE TABLE IF NOT EXISTS
                     modelo text DEFAULT null,
                     id_vendedora integer DEFAULT -1,
                     id_tipo integer NOT NULL,
-                    FOREIGN KEY (id_tipo) REFERENCES tipo(id)
-                    FOREIGN KEY (id_vendedora) REFERENCES vendedora(id)            
+                    FOREIGN KEY (id_tipo) REFERENCES tipo(id) ON DELETE CASCADE,
+                    FOREIGN KEY (id_vendedora) REFERENCES vendedora(id)  ON DELETE CASCADE          
                 )`
 //
 let queryCriaVenda = `CREATE TABLE IF NOT EXISTS
@@ -46,8 +48,8 @@ let queryCriaVenda = `CREATE TABLE IF NOT EXISTS
                 data text,
                 id_item integer NOT NULL,
                 id_vendedora integer NOT NULL,
-                FOREIGN KEY (id_item) REFERENCES item(id),
-                FOREIGN KEY (id_vendedora) REFERENCES vendedora(id)
+                FOREIGN KEY (id_item) REFERENCES item(id) ON DELETE CASCADE,
+                FOREIGN KEY (id_vendedora) REFERENCES vendedora(id) ON DELETE CASCADE
                 )`
 //
 
